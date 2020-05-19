c4512e5b846464008075318a3d53f03b173a6bab
Shulhan <muhammad.shulhan@tokenomy.com>
2020-05-19 20:12:43 +0700

    v2: add a simple test for private websocket

---
+++ v2/private_websocket_test.go
@@ -0,0 +1,35 @@
+// Copyright 2020 Tokenomy Technologies Ltd. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package v2
+
+import (
+	"os"
+	"testing"
+
+	"github.com/tokenomy/tokenomy-go"
+)
+
+func TestPrivateWebSocket_UserInfo(t *testing.T) {
+	if len(os.Getenv(envTestE2E)) == 0 {
+		t.Skipf("%s is not set, skipping ...", envTestE2E)
+	}
+	env := &tokenomy.Environment{
+		Address: os.Getenv(envAddress),
+		Token:   os.Getenv(envToken),
+		Secret:  os.Getenv(envSecret),
+	}
+
+	cl, err := NewPrivateWebSocket(env)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	userInfo, err := cl.UserInfo()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("UserInfo: %+v\n", userInfo)
+}
---
+++ v2/v2_test.go
@@ -0,0 +1,12 @@
+// Copyright 2020 Tokenomy Technologies Ltd. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package v2
+
+const (
+	envAddress = "TOKENOMY_ADDRESS"
+	envSecret  = "TOKENOMY_SECRET"
+	envTestE2E = "TOKENOMY_TEST_E2E"
+	envToken   = "TOKENOMY_TOKEN"
+)
