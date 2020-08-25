b48f760e346b5bd1d66f5e66568659064f2840fe
Shulhan <muhammad.shulhan@tokenomy.com>
2020-08-25 12:06:58 +0700

    v2/response: add methods to pack and unpack to/from gob format

--- v2/response.go
+++ v2/response.go
@@ -4,7 +4,13 @@
 
 package v2
 
-import liberrors "github.com/shuLhan/share/lib/errors"
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+
+	liberrors "github.com/shuLhan/share/lib/errors"
+)
 
 //
 // Response contains the HTTP response from server.
@@ -12,4 +18,30 @@ import liberrors "github.com/shuLhan/share/lib/errors"
 type Response struct {
 	liberrors.E
 	Data interface{} `json:"data,omitempty"`
+}
+
+//
+// PackGob pack the response into raw bytes using gob format.
+//
+func (res *Response) PackGob() (b []byte, err error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err = enc.Encode(res)
+	if err != nil {
+		return nil, fmt.Errorf("Response.Pack: %w", err)
+	}
+	return buf.Bytes(), nil
+}
+
+//
+// UnpackGob unpack the gob formatted raw bytes into Response.
+//
+func (res *Response) UnpackGob(b []byte) (err error) {
+	dec := gob.NewDecoder(bytes.NewReader(b))
+	res.Data = make([]byte, 0)
+	err = dec.Decode(res)
+	if err != nil {
+		return fmt.Errorf("Response.Unpack: %w", err)
+	}
+	return nil
 }
