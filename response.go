50bf9f10e072f956ec1bf32ce58187eb8ea547e5
Shulhan <muhammad.shulhan@tokenomy.com>
2020-10-13 17:56:52 +0700

    response: remove the PackGob and UnpackGob methods

--- response.go
+++ response.go
@@ -5,10 +5,6 @@
 package tokenomy
 
 import (
-	"bytes"
-	"encoding/gob"
-	"fmt"
-
 	liberrors "github.com/shuLhan/share/lib/errors"
 )
 
@@ -18,30 +14,4 @@ import (
 type Response struct {
 	liberrors.E
 	Data interface{} `json:"data,omitempty"`
-}
-
-//
-// PackGob pack the response into raw bytes using gob format.
-//
-func (res *Response) PackGob() (b []byte, err error) {
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	err = enc.Encode(res)
-	if err != nil {
-		return nil, fmt.Errorf("Response.Pack: %w", err)
-	}
-	return buf.Bytes(), nil
-}
-
-//
-// UnpackGob unpack the gob formatted raw bytes into Response.
-//
-func (res *Response) UnpackGob(b []byte) (err error) {
-	dec := gob.NewDecoder(bytes.NewReader(b))
-	res.Data = make([]byte, 0)
-	err = dec.Decode(res)
-	if err != nil {
-		return fmt.Errorf("Response.Unpack: %w", err)
-	}
-	return nil
 }
