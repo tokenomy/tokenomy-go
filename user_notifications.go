5601bd6774f50fef9e4f4b42237644e0e6a51329
yogi sofiharja <hardja.ogie@gmail.com>
2021-07-07 14:03:05 +0700

    user_notifications: add earn related product notification type

--- user_notifications.go
+++ user_notifications.go
@@ -10,8 +10,19 @@ package tokenomy
 // notification will be send to user.
 //
 type UserNotifications struct {
-	Deposit  bool `json:"deposit"`
-	Login    bool `json:"login"`
-	Trade    bool `json:"trade"`
-	Withdraw bool `json:"withdraw"`
+	UserID                 int64 `json:"-"`
+	Deposit                bool  `json:"deposit"`
+	Login                  bool  `json:"login"`
+	Withdraw               bool  `json:"withdraw"`
+	Order                  bool  `json:"order"`
+	FixedDeposit           bool  `json:"fixed_deposit"`
+	FixedDepositNearMature bool  `json:"fixed_deposit_near_mature"`
+	FixedDepositReward     bool  `json:"fixed_deposit_reward"`
+	DualCurrencyInvest     bool  `json:"dual_currency_invest"`
+	DualCurrencyReward     bool  `json:"dual_currency_reward"`
+	Staking                bool  `json:"staking"`
+	Unstaking              bool  `json:"unstaking"`
+	StakingReward          bool  `json:"staking_reward"`
+	Created                int64 `json:"-"`
+	Updated                int64 `json:"-"`
 }
