77f25dd05eade0fac7e5d192ac93605f8b6a7b74
yogi sofiharja <hardja.ogie@gmail.com>
2021-10-27 15:31:28 +0700

    all: Add Asset struct

---
+++ asset.go
@@ -0,0 +1,13 @@
+// Copyright 2021 Tokenomy Technologies Ltd. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package tokenomy
+
+//
+// Asset contains basic information of an asset
+//
+type Asset struct {
+	Name string `json:"name"`
+	Logo string `json:"logo"`
+}
